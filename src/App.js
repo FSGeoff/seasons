7e435fb8c656a5df2d985c1cb8e06c25160eb032
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-26 21:35:20 -0400

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
+import './styles/App.css'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {lat: null, errorMessage: ''};
 
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({lat: position.coords.latitude});
@@ -15,16 +18,25 @@ class App extends React.Component {
             }
         );
     };
+    renderContent() {
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        ;
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        ;
+        return <Spinner message={"Please accept location request..."}/>
+    }
 
     render() {
-
-           if(this.state.errorMessage && !this.state.lat) {
-               return <div>Error: {this.state.errorMessage}</div>
-           };
-           if(!this.state.errorMessage && this.state.lat) {
-             return   <div>Latitude: {this.state.lat}</div>
-        };
-           return <div>Loading... </div>
+    return (
+        <div>
+            {this.renderContent()}
+        </div>
+    )
 
     };
 };
